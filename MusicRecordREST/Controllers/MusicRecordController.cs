ea4e2983991c6d325220c007d124d6b74259d3cb
MissMaaai <nmtd10@hotmail.com>
2024-11-13 13:25:13 +0100

    tilføjet controller, tilføjet reference til projektet

---
+++ MusicRecordREST/Controllers/MusicRecordController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicRecordsRepository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicRecordREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicRecordController : ControllerBase
+    {
+        private readonly MusicRecordRepo _repo; 
+        // GET: api/<MusicRecordController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MusicRecordController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MusicRecordController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MusicRecordController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MusicRecordController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
